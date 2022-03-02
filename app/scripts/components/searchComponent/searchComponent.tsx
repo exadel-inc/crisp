cd25f27a4175511dadd5ad8f85bbcbc2660a4a5c
DmitryPateew <isildor.008@mail.ru>
2022-03-02 12:43:13 +0300

    add search button and styles

--- app/scripts/components/searchComponent/searchComponent.tsx
+++ app/scripts/components/searchComponent/searchComponent.tsx
@@ -1,18 +1,24 @@
 import * as React from 'react';
 import {useState} from 'react';
+import {BigButton} from '../bigButton/bigButton';
 
 export const SearchComponent = () => {
   const [value, setValue] = useState('');
 
+  const checkLengthValue = () => {
+    return value.length > 0;
+  };
+
   return (
-    <form>
+    <form className="searchForm">
       <input
         onChange={(e) => setValue(e.target.value)}
         className="search"
         placeholder="Search a framework, project, or element"
         type="search"
-        style={value.length > 0 ? {background: 'none'} : {}}
+        style={checkLengthValue() ? {background: 'none'} : {}}
       />
+      <BigButton buttonName='Search' disable={checkLengthValue()}/>
     </form>
   );
 };
