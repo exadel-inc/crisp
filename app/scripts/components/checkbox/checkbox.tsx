def59bf5bdd8cd9501cf6df5a12be72d34dcd490
DmitryPateew <isildor.008@mail.ru>
2022-03-11 20:37:10 +0100

    add constants

--- app/scripts/components/checkbox/checkbox.tsx
+++ app/scripts/components/checkbox/checkbox.tsx
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
+import {DEFAULT_SELECT_COUNT, MAIN_CHECKBOX_LABEL} from '../../constants/constants';
 import './chechbox.scss';
 
-export const Checkbox = ({count = 10}) => {
+export const Checkbox = ({count = DEFAULT_SELECT_COUNT}) => {
   const [checked, setChecked] = useState(false);
 
   return (
     <div className='checkBoxWrapper' onClick={() => setChecked(prevState => !prevState)}>
-      Select all {count}
+      {MAIN_CHECKBOX_LABEL} {count}
       {checked ?
         <div className='checkboxSquare checked'>
           <svg className='hoverIconFill' viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
