4a012bfe51c6273f4695d76e4a92a90b4ecfc859
DmitryPateew <isildor.008@mail.ru>
2022-03-11 20:29:59 +0100

    refactor common styles and checkbox styles

--- app/scripts/components/checkbox/checkbox.tsx
+++ app/scripts/components/checkbox/checkbox.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import './chechbox.scss';
 
 export const Checkbox = ({count = 10}) => {
   const [checked, setChecked] = useState(false);
@@ -8,12 +9,11 @@ export const Checkbox = ({count = 10}) => {
       Select all {count}
       {checked ?
         <div className='checkboxSquare checked'>
-          <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5.01493 9.49254L1.25373 5.73134L0 6.98507L5.01493 12L15.7612 1.25373L14.5075 0L5.01493 9.49254Z"
-                  fill="#0093FE"/>
+          <svg className='hoverIconFill' viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.01493 9.49254L1.25373 5.73134L0 6.98507L5.01493 12L15.7612 1.25373L14.5075 0L5.01493 9.49254Z"/>
           </svg>
         </div>
-        : <div className='checkboxSquare'></div>
+        : <div className='checkboxSquare'/>
       }
     </div>
   );
