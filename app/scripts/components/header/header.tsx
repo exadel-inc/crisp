b9d5567866a8fb8fce465a67f3a1942fccd6c88a
DmitryPateew <isildor.008@mail.ru>
2022-03-17 15:54:53 +0100

    add pill component

--- app/scripts/components/header/header.tsx
+++ app/scripts/components/header/header.tsx
@@ -6,6 +6,8 @@ import {NavigationTabs, NavigationTabType} from './navigation-tabs';
 import {Checkbox} from '../checkbox/checkbox';
 import {SearchComponent} from '../searchComponent/searchComponent';
 import {ProjectPanel} from '../projectPanel/projectPanel';
+import {Pill} from '../pill/pill';
+import {DEFAULT_PROJECT_PAGE_NAME} from '../../constants/constants';
 
 /**
  * Application Header
@@ -48,6 +50,7 @@ export function Header() {
         <Checkbox/>
         <div>
           <ProjectPanel projectName='Project001 web redisineskjnsdjknv' counter='2'/>
+          <Pill pageName={DEFAULT_PROJECT_PAGE_NAME}/>
         </div>
       </div>
     </header>
