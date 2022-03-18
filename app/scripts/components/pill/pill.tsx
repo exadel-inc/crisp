1daf542b81191bbbdf06b3c9d2ca34c329acacfa
DmitryPateew <isildor.008@mail.ru>
2022-03-18 10:29:46 +0100

    fix icons in separate components

--- app/scripts/components/pill/pill.tsx
+++ app/scripts/components/pill/pill.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import './pill.scss';
 import {useState} from 'react';
+import {CheckedIcon} from '../checkedIcon/checkedIcon';
+import {CloseIcon} from '../closeIcon/closeIcon';
+import {DEFAULT_PROJECT_PAGE_NAME} from '../../constants/constants';
 
-export const Pill = ({pageName ='string'}) => {
+export const Pill = ({pageName = DEFAULT_PROJECT_PAGE_NAME}) => {
   const [isActive, setIsActive] = useState(false);
   const [isRemove, setIsRemove] = useState(false);
 
@@ -11,16 +14,10 @@ export const Pill = ({pageName ='string'}) => {
   };
 
   return (
-    !isRemove ? <div onClick={changeActiveStatus} className={isActive ? 'pill active' : 'pill'}>
-      <p>{pageName}</p>
-      {
-        isActive ?
-          <svg viewBox="0 0 12 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3.73333 7.06667L0.933333 4.26667L0 5.2L3.73333 8.93333L11.7333 0.933333L10.8 0L3.73333 7.06667Z"/>
-          </svg>
-          : <svg onClick={() => setIsRemove(true)} viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M4.83429 4L8 7.16571V8H7.16571L4 4.83429L0.834286 8H0V7.16571L3.16571 4L0 0.834286V0H0.834286L4 3.16571L7.16571 0H8V0.834286L4.83429 4Z"/>
-          </svg>}
-    </div> : null);
+    !isRemove ?
+      <div onClick={changeActiveStatus} className={isActive ? 'pill active' : 'pill'}>
+        <p>{pageName}</p>
+        {isActive ? <CheckedIcon/> : <CloseIcon setIsRemove={setIsRemove}/>}
+      </div>
+      : null);
 };
