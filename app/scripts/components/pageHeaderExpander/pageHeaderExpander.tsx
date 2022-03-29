3a11a1d7c0a3a302cdf2095a6f20308dbe446795
DmitryPateew <isildor.008@mail.ru>
2022-03-29 10:24:39 +0200

    add page header expander

---
+++ app/scripts/components/pageHeaderExpander/pageHeaderExpander.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './pageHeaderExpander.scss';
+
+export const PageHeaderExpander = () => {
+  return (
+    <div className='expanderWrapper'>
+      <svg className='defaultIconFill' viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.94 0.393311L4 3.44664L7.06 0.393311L8 1.33331L4 5.33331L0 1.33331L0.94 0.393311Z"/>
+      </svg>
+    </div>
+  );
+};
--- app/scripts/components/pageTableHeader/pageTableHeader.tsx
+++ app/scripts/components/pageTableHeader/pageTableHeader.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {PAGE_TABLE_HEADER_COLUMN} from '../../constants/constants';
+import {PageHeaderExpander} from '../pageHeaderExpander/pageHeaderExpander';
 import './pageTableHeader.scss';
-
 
 export const PageTableHeader = () => {
   const {ELEMENT, PATTERN, PARAMETER, ACTIONS} = PAGE_TABLE_HEADER_COLUMN;
@@ -12,11 +12,7 @@ export const PageTableHeader = () => {
       <p>{PATTERN}</p>
       <p>{PARAMETER}</p>
       <p>{ACTIONS}</p>
-      <div>
-        <svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0.94 0.393311L4 3.44664L7.06 0.393311L8 1.33331L4 5.33331L0 1.33331L0.94 0.393311Z" fill="#C9C9C9"/>
-        </svg>
-      </div>
+      <PageHeaderExpander/>
     </div>
   );
 };
