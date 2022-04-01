9ef73e299f6ff7843e083be50d2362468292a85a
DmitryPateew <isildor.008@mail.ru>
2022-04-01 16:56:04 +0200

    create open tab element and refactor tab element

--- app/scripts/components/sortSelector/sortSelector.tsx
+++ app/scripts/components/sortSelector/sortSelector.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
-import {SORT_LABEL} from '../../constants/constants';
+import {SORT_LABEL, SORT_TYPES} from '../../constants/constants';
 import './sortSelector.scss';
 
 export const SortSelector = () => {
+  const options = Object.values(SORT_TYPES).map(option => <option value={option}>{option}</option>);
+
   return (
     <label className='projectSort'>
       {SORT_LABEL}
       <select>
-        <option>newest</option>
-        <option>oldest</option>
+        {options}
       </select>
     </label>
   );
