d882a9ad133e8efc9d86105daa1e1fc2aad31e94
Miatselski Dzmitry <dmiatselski@exadel.net>
2022-04-07 00:35:18 +0300

    feat(tooltip): add tooltip component

---
+++ app/scripts/components/tooltip/tooltip.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import './tooltip.less';
+
+interface TooltipProps {
+  children: React.ReactNode;
+  position: 'top-left' | 'top' | 'top-right' | 'right' | 'bottom-left' | 'bottom' | 'bottom-right' | 'left';
+  text: string;
+  tooltipStyle?: object;
+};
+
+/**
+ * Tooltip Component
+ */
+export function Tooltip ({ position, text, tooltipStyle, children }: TooltipProps) {
+
+  const [isHovered, setIsHovered] = React.useState(false);
+
+  return (
+    <div
+      className='tooltipWrapper'
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isHovered && (
+        <div
+          className={`tooltipBox position-${position}`}
+          style={tooltipStyle}
+        >
+          {text}
+        </div>
+      )}
+      {children}
+    </div>
+  );
+};
