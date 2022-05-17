81e082fe1041bb9d36908d80b9427caf8965abc4
DmitryPateew <isildor.008@mail.ru>
2022-05-17 09:34:09 +0200

    add logo component

---
+++ app/scripts/components/logoComponent/logoComponent.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {NavigationTabType} from '../header/navigation-tabs';
+import {useDispatch} from 'react-redux';
+import {NavigateAction} from '../../redux/reducers/navigation/navigation.actions';
+import './logoComponent.scss';
+
+export const LogoComponent = () => {
+  const dispatch = useDispatch();
+
+  const handleTabClick = (tab: NavigationTabType) => {
+    dispatch({...new NavigateAction(tab)});
+  };
+
+  return (
+    <div onClick={() => handleTabClick(NavigationTabType.MAIN)}
+         className="logo">
+      <span className='firstPart'>Cri</span>
+      <span className='lastPart'>sp</span>
+    </div>
+  );
+};
